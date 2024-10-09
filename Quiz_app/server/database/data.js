c2d6305a06ad0a0b58cdac419427f3bafebd6e70
Birvaa <155994695+Birvaa@users.noreply.github.com>
2024-10-09 17:50:33 +0530

    Add files via upload

---
+++ Quiz_app/server/database/data.js
@@ -0,0 +1,29 @@
+export default[
+    {
+        id: 1,
+        question: "What is the capital of France?",
+        options: ["Paris", "London", "Berlin"],
+    },
+    {
+        id: 2,
+        question: "What is the capital of Germany?",
+        options: ["Berlin", "Paris", "London"],
+    },
+    {
+        id: 3,
+        question: "What is the capital of the United Kingdom?",
+        options: ["London", "Paris", "Berlin"],
+    },
+    {
+        id: 4,
+        question: "What is the capital of Italy?",
+        options: ["Rome", "Paris", "Berlin"],
+    },
+    {
+        id: 5,
+        question: "What is the capital of Spain?",
+        options: ["Madrid", "Paris", "Berlin"],
+    }
+];
+
+export const answers = [0,1,2,1,0];
